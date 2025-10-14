0f9f584a5c1533c4f3405c544188b65e0aa0b163
Thiago Pereira <pereira.tp.thiago@gmail.com>
2025-10-13 23:09:54 -0300

    feat(error): classes e funcoes de erro padronizado

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
+import { AppError } from '@utils/errors/app-error'
 import cors from 'cors'
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import 'reflect-metadata'
 import './container'
 import routes from './routes'
@@ -18,5 +19,17 @@ app.use(cors(options))
 app.use(express.json())
 
 app.use(routes)
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      message: err.message,
+    })
+  }
+  return response.status(500).json({
+    status: 'error',
+    message: `Internal server error - ${err.message}`,
+  })
+})
 
 export default app
---
+++ backend/src/utils/errors/http.ts
@@ -0,0 +1,66 @@
+export class ServerError extends Error {
+  readonly error: Error
+  constructor (error?: Error) {
+    super(error.message)
+    this.error = {
+      name: 'ServerError',
+      message: error.message,
+      stack: error?.stack
+    }
+  }
+}
+
+export class UnauthorizedError extends Error {
+  readonly error: Error
+  constructor () {
+    super('Unauthorized')
+    this.error = {
+      name: 'UnauthorizedError',
+      message: 'Unauthorized'
+    }
+  }
+}
+
+export class ForbiddenError extends Error {
+  readonly error: Error
+  constructor () {
+    super('Access denied')
+    this.error = {
+      name: 'ForbiddenError',
+      message: 'Access denied'
+    }
+  }
+}
+
+export class NotFoundError extends Error {
+  readonly error: Error
+  constructor () {
+    super('Not found')
+    this.error = {
+      name: 'NotFoundError',
+      message: 'Not found'
+    }
+  }
+}
+
+export class SendMailError extends Error {
+  readonly error: Error
+  constructor () {
+    super('Send mail failure')
+    this.error = {
+      name: 'SendMailError',
+      message: 'Send mail failure'
+    }
+  }
+}
+
+export class ConflictError extends Error {
+  readonly error: Error
+  constructor (type: string) {
+    super('Conflict failure')
+    this.error = {
+      name: 'ConflictError',
+      message: `${type} em uso!`
+    }
+  }
+}
